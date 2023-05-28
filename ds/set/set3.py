ba2d140e0f82a054a0369941bec0239d0765426b
samirvithlani <samir.vithlani83955@gmail.com>
2023-05-28 15:21:09 +0530

    set

---
+++ ds/set/set3.py
@@ -0,0 +1,13 @@
+users = ["amit","anil","priya","parth","priya"]
+print(users)
+userset = set(users)
+users = list(userset)
+print(users)
+
+set1 = set({1,2,3,4,5,6,7,8,9,10})
+set2 = set({11,22,232,445,6758,960,-7,-8,9,10})
+users1 = [set1,set2]
+print(users1)
+
+data = set({[1,2,4],[2,3,4]})
+print(data)
