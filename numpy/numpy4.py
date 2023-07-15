6da6397f813ebdf32dd7892eba44457d13d427f3
samirvithlani <samir.vithlani83955@gmail.com>
2023-07-15 16:48:14 +0530

    ok

---
+++ numpy/numpy4.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+arr = np.array([[1,2,3,5],[4,5,6,8],[7,8,9,12]])
+arr = arr +10
+arr = arr * 2
+arr = arr / 2
+arr = arr - 10
+print(arr)
+
+arr1 = np.array([[1,2,3,5],[4,5,6,8]])
+arr2 = np.array([[1,2,3,5],[4,5,6,8]])
+
+arr3 = arr1 + arr2
+print(arr3)
